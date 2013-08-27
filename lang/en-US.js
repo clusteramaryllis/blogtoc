b6be9b369f8d680b6e63e5859cece1c16bf04a94
clusteramaryllis <clusteramaryllis77@gmail.com>
2013-08-28 04:08:18 +0700

    fix author name, change indentation to spaces

--- lang/en-US.js
+++ lang/en-US.js
@@ -1,51 +1,51 @@
 // BlogToc language configuration
 // language : English (America)
-// author : Cluster Amarylis
+// author : Cluster Amaryllis
 
 (function(){
-	
-	var loadLang = function( BlogToc ) {
-		(function(){
+  
+  var loadLang = function( BlogToc ) {
+    (function(){
 
-			/*****************************************************************
-			 * You can change these lines                                    *
-			 *****************************************************************/
-			BlogToc.language( 'en-US', {
-				labelAll: 'All',
-				newLabel: 'New',
-				index: '#',
-				thumbnail: 'Thumb',
-				title: 'Titles',
-				author: 'Authors',
-				comment: 'Comments',
-				publishDate: 'Published',
-				updateDate: 'Updated',
-				summary: 'Summaries',
-				display: ' records per page',
-				search: 'Search :',
-				noRecords: 'No matching records found',
-				result: 'Showing {begin} to {end} out of {total}',
-				firstPage: '&laquo; First',
-				lastPage: 'Last &raquo;',
-				prevPage: '&lsaquo; Prev',
-				nextPage: 'Next &rsaquo;',
-				errorMessage: 'This error message is part of BlogToc application & occurred because one of following reasons :' + 
-				  '\n' +
-				  '\n • The URL you provide is not valid.' +
-				  '\n • The URL you provide is not a blogspot service.' +
-				  '\n • The blog is a private blog.' +
-				  '\n • The blog has been deleted.' +
-				  '\n • There is a trouble in your internet connection.'
-			});
-			/*****************************************************************
-			 * End changing lines                                            *
-			 *****************************************************************/
+      /*****************************************************************
+       * You can change these lines                                    *
+       *****************************************************************/
+      BlogToc.language( 'en-US', {
+        labelAll: 'All',
+        newLabel: 'New',
+        index: '#',
+        thumbnail: 'Thumb',
+        title: 'Titles',
+        author: 'Authors',
+        comment: 'Comments',
+        publishDate: 'Published',
+        updateDate: 'Updated',
+        summary: 'Summaries',
+        display: ' records per page',
+        search: 'Search :',
+        noRecords: 'No matching records found',
+        result: 'Showing {begin} to {end} out of {total}',
+        firstPage: '&laquo; First',
+        lastPage: 'Last &raquo;',
+        prevPage: '&lsaquo; Prev',
+        nextPage: 'Next &rsaquo;',
+        errorMessage: 'This error message is part of BlogToc application & occurred because one of following reasons :' + 
+          '\n' +
+          '\n • The URL you provide is not valid.' +
+          '\n • The URL you provide is not a blogspot service.' +
+          '\n • The blog is a private blog.' +
+          '\n • The blog has been deleted.' +
+          '\n • There is a trouble in your internet connection.'
+      });
+      /*****************************************************************
+       * End changing lines                                            *
+       *****************************************************************/
 
-		})();
-	}
+    })();
+  }
 
-	if ( typeof window !== 'undefined' && window.BlogToc ) {
-		loadLang( window.BlogToc );
-	}
+  if ( typeof window !== 'undefined' && window.BlogToc ) {
+    loadLang( window.BlogToc );
+  }
 
 })();
