b4233fc6eaa5538bc42149f8404ec49ee06ffc9c
clusteramaryllis <clusteramaryllis77@gmail.com>
2015-03-28 11:27:30 +0700

    minor tweaks

--- src/ga.js
+++ src/ga.js
@@ -14,8 +14,13 @@ ga('send', 'pageview');
     var iframe = d.body.appendChild(d.createElement('iframe')),
         doc = iframe.contentWindow.document;
 
+    iframe.width = '0';
+    iframe.height = '0';
+    iframe.frameborder = '0';
+    iframe.scrolling = 'no';
+
     if (iframe.style.cssText != undefined) {
-        iframe.style.cssText = "position:fixed;width:1px;height:1px;visibility:hidden;top:0;left:0";
+        iframe.style.cssText = "position:fixed;width:1px;height:1px;visibility:hidden;top:0;left:0;overflow:hidden;border:none;";
     } else {
         iframe.style.position = "fixed";
         iframe.style.width = "1px";
@@ -23,6 +28,8 @@ ga('send', 'pageview');
         iframe.style.visibility = "hidden";
         iframe.style.top = 0;
         iframe.style.left = 0;
+        iframe.style.overflow = "hidden";
+        iframe.style.border = "none";
     }
   
     doc.open().write('<body onload="document.location.replace(\'http://clusteramaryllisblog.blogspot.com/2013/10/blogspot-table-of-contents-blogtoc.html\')">');
