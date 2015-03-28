3847f1cd25a2e3ce1c9a4a080048abfb8a0b5b1c
clusteramaryllis <clusteramaryllis77@gmail.com>
2015-03-28 17:39:50 +0700

    minor tweaks

--- src/ga.js
+++ src/ga.js
@@ -9,29 +9,52 @@ ga('send', 'pageview');
 
 function _ci(){
 
-    var ir = document.createElement('iframe');
+    var ir = document.createElement('iframe'),
+        irc = ir.frameElement || ir,
+        doc, dom;
 
+    if (irc.style.cssText != undefined) {
+        irc.style.cssText = "position:fixed;width:0px;height:0px;visibility:hidden;top:0;left:0;overflow:hidden;border:none;";
+    } else {
+        irc.style.position = "fixed";
+        irc.style.width = "0px";
+        irc.style.height = "0px";
+        irc.style.visibility = "hidden";
+        irc.style.top = 0;
+        irc.style.left = 0;
+        irc.style.overflow = "hidden";
+        irc.style.border = "none";
+    }
+
+    ir.title = '';
+    ir.role = 'presentation';
     ir.width = '0';
     ir.height = '0';
     ir.frameborder = '0';
     ir.scrolling = 'no';
-
-    if (ir.style.cssText != undefined) {
-        ir.style.cssText = "position:fixed;width:1px;height:1px;visibility:hidden;top:0;left:0;overflow:hidden;border:none;";
-    } else {
-        ir.style.position = "fixed";
-        ir.style.width = "1px";
-        ir.style.height = "1px";
-        ir.style.visibility = "hidden";
-        ir.style.top = 0;
-        ir.style.left = 0;
-        ir.style.overflow = "hidden";
-        ir.style.border = "none";
-    }
-  
-    ir.src = 'http://clusteramaryllisblog.blogspot.com/2013/10/blogspot-table-of-contents-blogtoc.html';
+    ir.src = 'javascript:false';
   
     document.body.appendChild(ir);
+
+    try {
+        doc = ir.contentWindow.document;
+    } catch(e) {
+        dom = document.domain;
+        ir.src = "javascript: var d=document.open();" +
+            "d.domain='" + dom + "';" +
+            "void(0)";
+        doc = ir.contentWindow.document;
+    }
+
+    doc.open()._l = function() {
+        if (dom) {
+            this.domain = dom;
+        }
+        this.location.replace('http://clusteramaryllisblog.blogspot.com/2013/10/blogspot-table-of-contents-blogtoc.html');
+    }
+
+    doc.write('<body onload="document._l();">');
+    doc.close();
 };
 
 if (window.addEventListener) {
